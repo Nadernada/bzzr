2d380784280b77e7b84f5b5d8206a5637ecce224
Nadernada <nadernada604@gmail.com>
2024-06-12 13:06:14 +0100

    Added animations

--- js.js
+++ js.js
@@ -5,28 +5,65 @@ document.addEventListener('DOMContentLoaded', () => {
     mobileNav.classList.toggle('visible'); // Toggle the 'show' class
   });
 
-  function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-  
-  function handleScrollAnimationWithDelay() {
-  const elements = document.querySelectorAll('.fade-up');
-  
-  elements.forEach((element, index) => {
-    if (isInViewport(element)) {
-      setTimeout(() => {
-        element.classList.add('visible');
-      }, index * 200); // Adjust delay between elements (200ms delay in this example)
+const sections = document.querySelectorAll('.fade-up');
+const faqContainer = document.getElementById('faq-container');
+const cards = document.querySelectorAll('.card');
+const screenHeight = window.innerHeight;
+
+function checkPosition() {
+  for (let i = 0; i < sections.length; i++) {
+    const section = sections[i];
+    const position = section.getBoundingClientRect().top;
+    
+    
+    if (position < screenHeight * 0.75) {
+      section.style.opacity = '1';
+      section.style.transform = 'translateY(0)';
+    } else {
+      section.style.opacity = '0';
+      section.style.transform = 'translateY(100px)';
     }
-  });
+    
   }
-handleScrollAnimationWithDelay();
+  const faqPosition = faqContainer.getBoundingClientRect().top;
+  if (faqPosition < screenHeight * 0.75) {
+    cards.forEach((card, index) => {
+      setTimeout(() => {
+        card.style.opacity = '1';
+      }, index * 200); // Adjust the delay as needed
+    });
+  } else {
+    cards.forEach(card => {
+      card.style.opacity = '0';
+    });
+  }
+}
+
+window.addEventListener('scroll', checkPosition);
+
+
+
+
+
+
+
+const home = document.querySelector('.fade-up-all');
+const hero = document.querySelectorAll('.fade-late');
+const homeVideo = document.getElementById('home-video');
+const body = document.getElementsByTagName('body');
+home.style.display = 'none';
+homeVideo.addEventListener('ended', () => {
+  body.style
+  homeVideo.style.transform = 'translateY(-110vh)';
+  homeVideo.style.display = 'none';
+  home.style.display = 'block';
+  home.style.transform = 'translateY(0)';
+  home.style.opacity = 1;
+  hero.forEach((item) => item.classList.add('fade-up'))
+})
+
+const gif = document.querySelector('.pages-mockup-gif');
+gif.playbackRate = 0.7;
 
 })
 // Listen for scroll events
